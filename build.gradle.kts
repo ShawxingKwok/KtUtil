cb8f449a65abd57563ca354a1c9ee3e3d1caa866
WilliamKwokX <1425183716@qq.com>
2023-07-24 22:18:17 +0800

    +

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ kotlin {
         hostOs == "Mac OS X" -> macosX64("native")
         hostOs == "Linux" -> linuxX64("native")
         hostOs.startsWith("Windows") -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+        else -> throw GradleException("Host OS $hostOs is not supported in Kotlin/Native.")
     }
     .binaries.executable { entryPoint = "main" }
 
